7520bbaa4e39ab82b2b3102382abd33c3f2dc260
university <university@gmail.com>
2018-05-05 14:20:38 -0500

    mi proyecto hoja de vida

---
+++ formulario.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+	<title>Hoja de Vida</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo2.css">
+</head>
+<body>
+<div class="T1">
+<h1>Hoja De Vida</h1>
+</div>
+<?php 
+
+ $Cuidad=$_GET['Cuidad'];
+ $Papellido=$_GET['Primer apellido'];
+ $Sapellido=$_GET['Segundo apellido'];
+ $Nombre=$_GET['Nombre'];
+ 
+ echo $Papellido;
+ echo $Sapellido;
+ echo $Nombre;
+ 
+ 
+
+ 
+
+
+
+ ?>
+</body>
+</html>
